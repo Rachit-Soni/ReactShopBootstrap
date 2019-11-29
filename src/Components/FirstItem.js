c37ae3c1961e326b3b85745cb09c5671ebe18d61
Rachit-Soni <57813172+Rachit-Soni@users.noreply.github.com>
2019-11-29 11:46:58 +0530

    background img and state

--- src/Components/FirstItem.js
+++ src/Components/FirstItem.js
@@ -8,16 +8,14 @@ export default class FirstItem extends Component {
     state = {
                 loading: true,
                 firstApiData: [],        
-                cartState: [
-                                {addedItemName: [] },
-                                {addedItemQty: 0 }
-                           ] 
-
+                cartState: {
+                                    name : '',
+                                    price: '',
+                                    quantity: 0
+                                }
             };
 
-    
-
-    
+    //componentDidMount     
     async componentDidMount() {
         const url = 'https://api.myjson.com/bins/1hj9cq';
         const response = await fetch (url);
@@ -27,23 +25,44 @@ export default class FirstItem extends Component {
     }
 
 
+    //Functions
+ qty = 1;
+    updateItem = (event) =>{
+        ++this.qty;
+        console.log(this.qty);
+    }
+
+
+   btnCart = (event) => {
+       console.log('Button Works!')
+       console.log(this.cartState);
+       this.setState({cartState: 
+                        {name:this.state.firstApiData.name,
+                        quantity: this.qty,
+                        price: this.state.firstApiData.price * this.qty}
+                    })
+       console.log(this.cartState);             
+   }
     
-    
+/*     function btnCart (event){
+        console.log('Button Works!')
+       
+        console.log(this.state.firstApiData);
+
+
+};
+
+    let qty=0;
+    function updateItem(event) {
+        ++qty;
+        console.log(qty)
+    }
+
+ */
+
     render() {
 
-        function btnCart (event){
-            console.log('Button Works!')
-           
-            console.log(this.state.firstApiData);
-
-
-    };
-    
-        let qty=0;
-        function updateItem(event) {
-            ++qty;
-            console.log(qty)
-        }
+      
         
 
         return (
@@ -54,8 +73,8 @@ export default class FirstItem extends Component {
                         <p>{this.state.firstApiData.description}</p>
                         <img alt = "FirstProduct" src= {this.state.firstApiData.master.images[1]["product_url"]} />
                         <p>Price: {this.state.firstApiData.price}$</p>
-                        <input type = "number" name="quantity" min="1" max="5" value="1" onChange={updateItem}></input>    
-                        <button className="cartButton" onClick={btnCart}>Add to Cart</button>   
+                        <input type = "number" name="quantity" min="1" max="5" onChange={this.updateItem}></input>    
+                        <button className="cartButton" onClick={this.btnCart}>Add to Cart</button>   
                         <br />
                         <hr />
                     </div>
